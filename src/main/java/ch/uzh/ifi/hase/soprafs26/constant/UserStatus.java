86639dab0a531217eb54fa76e8502fe5f41b376e
daniloruggieri <danilo.ruggieri@bluewin.ch>
2026-04-02 13:57:36 +0200

    #25 implemented PUT endpoint and necessary DTO/UserService functions

--- src/main/java/ch/uzh/ifi/hase/soprafs26/constant/UserStatus.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/constant/UserStatus.java
@@ -2,4 +2,13 @@ package ch.uzh.ifi.hase.soprafs26.constant;
 
 public enum UserStatus {
 	ONLINE, OFFLINE;
+
+	public static UserStatus fromString(String status) {
+		for (UserStatus userStatus : UserStatus.values()) {
+			if (userStatus.name().equalsIgnoreCase(status)) {
+				return userStatus;
+			}
+		}
+		throw new IllegalArgumentException("Invalid user status: " + status);
+	}
 }
