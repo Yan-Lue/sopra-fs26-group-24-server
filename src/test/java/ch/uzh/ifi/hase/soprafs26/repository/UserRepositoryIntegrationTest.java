936b12a2d77e264e092f033f11afe8a0765f2e7b
Noel Schneuwly <noel.schneuwly@bluewin.ch>
2026-04-03 12:29:52 +0200

    #110 add additional repository tests

--- src/test/java/ch/uzh/ifi/hase/soprafs26/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs26/repository/UserRepositoryIntegrationTest.java
@@ -11,6 +11,8 @@ import ch.uzh.ifi.hase.soprafs26.entity.User;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeEach;
+
 @DataJpaTest
 class UserRepositoryIntegrationTest {
 
@@ -20,19 +22,24 @@ class UserRepositoryIntegrationTest {
 	@Autowired
 	private UserRepository userRepository;
 
-	@Test
-	void findByName_success() {
-		// given
-		User user = new User();
+	private User user;
+
+	@BeforeEach
+	void setup() {
+		user = new User();
 		user.setName("Firstname Lastname");
 		user.setUsername("firstname@lastname");
 		user.setStatus(UserStatus.OFFLINE);
 		user.setToken("1");
-        user.setPassword("password");
-        user.setEmail("firstname@lastname");
+		user.setPassword("password");
+		user.setEmail("firstname@lastname");
 
 		entityManager.persist(user);
 		entityManager.flush();
+	}
+
+	@Test
+	void findByName_success() {
 
 		// when
 		User found = userRepository.findByName(user.getName());
@@ -43,6 +50,42 @@ class UserRepositoryIntegrationTest {
 		assertEquals(found.getUsername(), user.getUsername());
 		assertEquals(found.getToken(), user.getToken());
 		assertEquals(found.getStatus(), user.getStatus());
-        assertEquals(found.getEmail(), user.getEmail());
+		assertEquals(found.getEmail(), user.getEmail());
+	}
+
+	@Test
+	void findByToken_success() {
+		User found = userRepository.findByToken(user.getToken());
+
+		assertNotNull(found.getId());
+		assertEquals(found.getName(), user.getName());
+		assertEquals(found.getUsername(), user.getUsername());
+		assertEquals(found.getToken(), user.getToken());
+		assertEquals(found.getStatus(), user.getStatus());
+		assertEquals(found.getEmail(), user.getEmail());
+	}
+
+	@Test
+	void findByEmail_success() {
+		User found = userRepository.findByEmail(user.getEmail());
+
+		assertNotNull(found.getId());
+		assertEquals(found.getName(), user.getName());
+		assertEquals(found.getUsername(), user.getUsername());
+		assertEquals(found.getToken(), user.getToken());
+		assertEquals(found.getStatus(), user.getStatus());
+		assertEquals(found.getEmail(), user.getEmail());
+	}
+
+	@Test
+	void findByUsername_success() {
+		User found = userRepository.findByUsername(user.getUsername());
+
+		assertNotNull(found.getId());
+		assertEquals(found.getName(), user.getName());
+		assertEquals(found.getUsername(), user.getUsername());
+		assertEquals(found.getToken(), user.getToken());
+		assertEquals(found.getStatus(), user.getStatus());
+		assertEquals(found.getEmail(), user.getEmail());
 	}
 }
