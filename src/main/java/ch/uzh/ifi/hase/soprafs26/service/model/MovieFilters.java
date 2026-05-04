d9fe35c639282f2578fd058bd6f00312a7a5fcff
Noel Schneuwly <noel.schneuwly@bluewin.ch>
2026-05-04 08:38:33 +0200

    #202 add functionality to filter for specific movie providers

--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/model/MovieFilters.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/model/MovieFilters.java
@@ -6,13 +6,14 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public record MovieFilters(
         List<Long> genreIds,
         Double minRating,
         LocalDate minReleaseYear,
-        LocalDate maxReleaseYear
-) {
+        LocalDate maxReleaseYear,
+        String providerIds) {
     private static final Map<String, Long> GENRE_NAME_TO_ID = Map.ofEntries(
             Map.entry("Action", 28L),
             Map.entry("Adventure", 12L),
@@ -32,15 +33,28 @@ public record MovieFilters(
             Map.entry("TV Movie", 10770L),
             Map.entry("Thriller", 53L),
             Map.entry("War", 10752L),
-            Map.entry("Western", 37L)
-    );
+            Map.entry("Western", 37L));
+
+    // we can also extend this, in case wwe have more provider in the future
+    private static final Map<String, String> PROVIDER_MAP = Map.of(
+            "Netflix", "8",
+            "DisneyPlus", "337",
+            "AppleTV", "350",
+            "AmazonPrime", "9",
+            "ParamountPlus", "531");
 
     public static MovieFilters fromDTO(SessionFilterPutDTO dto) {
-        List<Long> genreIds = dto.getGenres() == null ? List.of() : dto.getGenres()
-                                                                    .stream()
-                                                                    .map(GENRE_NAME_TO_ID::get)
-                                                                    .filter(Objects::nonNull)
-                                                                    .toList();
+        List<Long> genreIds = dto.getGenres() == null ? List.of()
+                : dto.getGenres()
+                        .stream()
+                        .map(GENRE_NAME_TO_ID::get)
+                        .filter(Objects::nonNull)
+                        .toList();
+        String providerIds = (dto.getProviders() == null || dto.getProviders().isEmpty()) ? null
+                : dto.getProviders().stream()
+                        .map(PROVIDER_MAP::get)
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.joining("|"));
 
         LocalDate minReleaseYear = null;
         LocalDate maxReleaseYear = null;
@@ -52,7 +66,6 @@ public record MovieFilters(
         if (dto.getMaxReleaseYear() != null) {
             maxReleaseYear = LocalDate.of(dto.getMaxReleaseYear(), 12, 31);
         }
-        return new MovieFilters(genreIds, dto.getMinRating(), minReleaseYear, maxReleaseYear);
+        return new MovieFilters(genreIds, dto.getMinRating(), minReleaseYear, maxReleaseYear, providerIds);
     }
 }
-
